c61bf83b7c275280793b323360e79cda286b27fa
Gabrielle Pignatari <gabriellepignatari@gmail.com>
2024-04-13 00:16:13 -0300

    front solicitações ong finalizado

--- src/pages/Configuracoes_ONG/components/Solicitacoes/Solicitacoes.js
+++ src/pages/Configuracoes_ONG/components/Solicitacoes/Solicitacoes.js
@@ -7,25 +7,55 @@ import foto_perfil from './icons/user_female.webp';
 
 function Solicitacoes() {
   return (
-    <div className='container__candidatura'>
-        <div className='info_candidatura'>
-            <img src={foto_perfil} alt="icon_candidato"/>
-            <div className='container__dadosCandidato'>
-                <span>
-                    <p className='nome_candidato'>Gabrielle Fantinati Pignatari</p>
-                    <p>Data de nascimento: 08/06/2000</p>
-                    <p>Gênero: Menina Mulher</p>
-                </span>
-            </div>
-        </div>
-        <div className='status_candaditura'>
+    <>
+        <div className='cabecalho__candidatura'>
+            <div><p>CANDIDATOS</p></div>
             <div>
-                <FontAwesomeIcon icon={ faCircle } />
-                EM ANÁLISE
+                <select>
+                    <option>FILTRAR</option>
+                </select>
             </div>
-            <button>VISUALIZAR FORMULÁRIO</button>
         </div>
-    </div>
+        <hr className='hr_solicitacoes' />
+        <div className='container__candidato'>
+            <div className='info_candidatura'>
+                <img src={foto_perfil} alt="icon_candidato"/>
+                <div className='container__dadosCandidato'>
+                    <span>
+                        <p className='nome_candidato'>Gabrielle Fantinati Pignatari</p>
+                        <p>Data de nascimento: 08/06/2000</p>
+                        <p>Gênero: Menina Mulher</p>
+                    </span>
+                </div>     
+            </div>
+            <div className='status_candaditura'>
+                    <div className='status analise'>
+                        <FontAwesomeIcon icon={ faCircle } />
+                        <span>EM ANÁLISE</span>
+                    </div>
+                    <button>VISUALIZAR FORMULÁRIO</button>
+            </div>
+        </div>
+        <div className='container__candidato'>
+            <div className='info_candidatura'>
+                <img src={foto_perfil} alt="icon_candidato"/>
+                <div className='container__dadosCandidato'>
+                    <span>
+                        <p className='nome_candidato'>Antonio Victor Lopes Silva</p>
+                        <p>Data de nascimento: 19/03/2001</p>
+                        <p>Gênero: Masculino</p>
+                    </span>
+                </div>
+            </div>
+            <div className='status_candaditura'>
+                <div className='status aprovado'>
+                    <FontAwesomeIcon icon={ faCircle } />
+                    <span>APROVADO</span>
+                </div>
+                <button>VISUALIZAR FORMULÁRIO</button>
+            </div>
+        </div>
+        </>
   )
 }
 

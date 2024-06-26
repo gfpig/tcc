00d2ca6acf1d7539f63b44b68385a88992b25452
Gabrielle Pignatari <gabriellepignatari@gmail.com>
2024-06-26 00:32:35 -0300

    [HOME] Adicionando descrição dos devs ao footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './footer.css';
-import ana from '../../assets/devs/ana_giulia.jpg';
+import ana from '../../assets/devs/ana_giulia.png';
 import antonio from '../../assets/devs/antonio.jpg';
-import flavia from '../../assets/devs/flavia.jpg';
+import flavia from '../../assets/devs/flavia.jpeg';
 import gabrielle from '../../assets/devs/gabrielle.jpg';
 
 const Footer = () => {
@@ -15,28 +15,28 @@ const Footer = () => {
                         <img src={ana} alt="" />
                         <h3>Ana Giulia Senra Nascimento</h3>
                     </div>
-                    <p>Breve descrição sobre o perfil da pessoa e quem é a Ana</p>
+                    <p><center>20 anos. Tecnica em administração e Administradora de banco de dados pela ETEC.</center></p>
                 </div>
                 <div className='dev2'>
                     <div className='foto'>
                         <img src={antonio} alt="" />
                         <h3>Antonio Victor Lopes Silva</h3>
                     </div>
-                    <p>Breve descrição sobre o perfil da pessoa e quem é o Antonio</p>
+                    <p><center>23 anos, apaixonado por dados e de games, pai de pet.</center></p>
                 </div>
                 <div className='dev3'>
                     <div className='foto'>
                         <img src={flavia} alt="" />
                         <h3>Flávia Conceição Ramos</h3>
                     </div>
-                    <p>Breve descrição sobre o perfil da pessoa e quem é a Flávia</p>
+                    <p><center>24 anos, apaixonada por tecnologia. Resiliente, persistente e curiosa, sempre em busca de novos desafios e conhecimentos.</center></p>
                 </div>
                 <div className='dev4'>
                     <div className='foto'>
                         <img src={gabrielle} alt="" />
                         <h3>Gabrielle Fantinati Pignatari</h3>
                     </div>
-                    <p>Breve descrição sobre o perfil da pessoa e quem é a Gabrielle</p>
+                    <p><center>24 anos, amante de programação front-end, mãe de pet e cronicamente online</center></p>
                 </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createClient } from "@supabase/supabase-js";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Login from './pages/Login/Login'
@@ -14,12 +13,6 @@ import Redefinir_Senha from './pages/Redefinir_Senha/Redefinir_Senha';
 
 import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 
-/*const PROJECT_URL = "https://xljeosvrbsygpekwclan.supabase.co";
-const PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhsamVvc3ZyYnN5Z3Bla3djbGFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ1MTY1NzAsImV4cCI6MjAzMDA5MjU3MH0.InFDrSOcPxRe4LXMBJ4dT59bBb3LSpKw063S90E3uPo"
-const supabase = createClient(PROJECT_URL, PUBLIC_KEY);*/
-
-//const [tipoSessao, setTipoSessao] = useState()
-
 const router = createBrowserRouter([
   {
     path: "/",
@@ -27,27 +20,27 @@ const router = createBrowserRouter([
   },
   {
     path: "login",
-    element: <Login />
+    element: (
+        <Login />
+    )
   },
   {
     path: "cadastro",
-    element: <Cadastro />
+    element: (
+        <Cadastro />
+    )
   },
   {
     path: "configuracoes_ong",
-    element: //(
-      //<PrivateRoute>
+    element: (
         <Configuracoes_ONG />
-      //</PrivateRoute>
-    //)
+    )
   },
   {
     path: "configuracoes_candidato",
-    element: //(
-      //<PrivateRoute>
+    element: (
         <Configuracoes_Candidato />
-      //</PrivateRoute>
-    //)
+    )
   },
   {
     path: "resultado",
@@ -55,46 +48,15 @@ const router = createBrowserRouter([
   },
   {
     path: "perfil_instituicao",
-    element: //(
-      //<PrivateRoute>
+    element: (
         <Perfil_ONG />
-      //</PrivateRoute>
-    //)
+    )
   },
   {
     path: "login/redefinir_senha",
     element: <Redefinir_Senha />
   }
 ]);
-
-/*function PrivateRoute({ children }) {
-  const [isAuthenticated, setIsAuthenticated] = useState(null);
-  useEffect(() => {
-    verificaSessao().then((result) => {
-      setIsAuthenticated(result);
-    });
-  }, []);
-
-  console.log(isAuthenticated)
-
-  return isAuthenticated === "deslogado" ? children : <Navigate to="/" />;
-
-}
-
-const verificaSessao = async () => { //pegando a sessão e colocando numa variável
-  const { data: { session }} = await supabase.auth.getSession();
-
-  console.log(session)
-  if(session === null) {
-    return "deslogado"
-  }
-
-  if (session.user.user_metadata.cpf === undefined) { //se for instituição
-      return "instituicao"
-  } else { //se for candidato
-    return "candidato"
-  }
-}*/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
